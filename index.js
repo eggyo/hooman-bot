bf7bbecc02fe248c5ba8d08f48de12240e5bfa25
eggyo <eggyoyo4859@gmail.com>
2017-05-31 12:06:44 +0700

    v.1

--- index.js
+++ index.js
@@ -23,44 +23,40 @@ var botParser = bot.parser();
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+    console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  restAPIKey: process.env.REST_KEY,
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'myAppId',
+    restAPIKey: process.env.REST_KEY,
+    masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+    liveQuery: {
+        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    }
 });
 var allowInsecureHTTP = true;
 var dashboard = new ParseDashboard({
-  "apps": [
-    {
-      "serverURL": "http://bot-afaps.herokuapp.com/parse",
-      "appId": "myAppId",
-      "masterKey": "myMasterKey",
-      "appName": "bot-afaps"
-    }
-  ],
-  "users": [
-    {
-      "user":"admin",
-      "pass":"pass"
-    }
-    ]
+    "apps": [{
+        "serverURL": "http://bot-afaps.herokuapp.com/parse",
+        "appId": "myAppId",
+        "masterKey": "myMasterKey",
+        "appName": "bot-afaps"
+    }],
+    "users": [{
+        "user": "admin",
+        "pass": "pass"
+    }]
 }, allowInsecureHTTP);
 
 function containsAny(str, substrings) {
     for (var i = 0; i != substrings.length; i++) {
-       var substring = substrings[i];
-       if (str.indexOf(substring) != - 1) {
-         return substring;
-       }
+        var substring = substrings[i];
+        if (str.indexOf(substring) != -1) {
+            return substring;
+        }
     }
     return null;
 }
@@ -74,64 +70,87 @@ function containsAny(str, substrings) {
 var app = express();
 app.post('/linewebhook', botParser);
 
-bot.on('message', function (event) {
-	switch (event.message.type) {
-		case 'text':
-    event.reply(event.message.text).then(function (data) {
-        // success
-    }).catch(function (error) {
-        // error
-    });
+bot.on('message', function(event) {
+    switch (event.message.type) {
+        case 'text':
+            var messageText = event.message.text;
+            processMessage(messageText, function(responseMsg) {
+                if (responseMsg == messageText) {
+                    callParseServerCloudCode("getReplyMsg", '{"msg":"' + messageText + '"}', function(response) {
+                        if (response == "") {
+                            console.log("no msg reply");
+                            event.reply("ข้าไม่เข้าใจที่เจ้าพูด").then(function(data) {
+                                // success
+                            }).catch(function(error) {
+                                // error
+                            });
+                        } else {
+                            event.reply(response).then(function(data) {
+                                // success
+                            }).catch(function(error) {
+                                // error
+                            });
+                        }
+                    });
+                } else {
+                    event.reply(responseMsg).then(function(data) {
+                        // success
+                    }).catch(function(error) {
+                        // error
+                    });
+                }
+            });
 
-			break;
-		case 'image':
 
-			break;
-		case 'video':
+            break;
+        case 'image':
 
-			break;
-		case 'audio':
-			//event.reply('Nice song!');
-			break;
-		case 'location':
-			//event.reply(['That\'s a good location!', 'Lat:' + event.message.latitude, 'Long:' + event.message.longitude]);
-			break;
-		case 'sticker':
-    /*
+            break;
+        case 'video':
+
+            break;
+        case 'audio':
+            //event.reply('Nice song!');
+            break;
+        case 'location':
+            //event.reply(['That\'s a good location!', 'Lat:' + event.message.latitude, 'Long:' + event.message.longitude]);
+            break;
+        case 'sticker':
+            /*
 			event.reply({
 				type: 'sticker',
 				packageId: 1,
 				stickerId: 1
 			});*/
-			break;
-		default:
-      console.log('unkwon type! :' + JSON.stringify(event));
-			break;
-	}
+            break;
+        default:
+            console.log('unkwon type! :' + JSON.stringify(event));
+            break;
+    }
 });
 
-bot.on('follow', function (event) {
-	//event.reply('follow: ' + event.source.userId);
+bot.on('follow', function(event) {
+    //event.reply('follow: ' + event.source.userId);
 });
 
-bot.on('unfollow', function (event) {
-	//event.reply('unfollow: ' + event.source.userId);
+bot.on('unfollow', function(event) {
+    //event.reply('unfollow: ' + event.source.userId);
 });
 
-bot.on('join', function (event) {
-///	event.reply('join: ' + event.source.groupId);
+bot.on('join', function(event) {
+    ///	event.reply('join: ' + event.source.groupId);
 });
 
-bot.on('leave', function (event) {
-//	event.reply('leave: ' + event.source.groupId);
+bot.on('leave', function(event) {
+    //	event.reply('leave: ' + event.source.groupId);
 });
 
-bot.on('postback', function (event) {
-	event.reply('postback: ' + event.postback.data);
+bot.on('postback', function(event) {
+    event.reply('postback: ' + event.postback.data);
 });
 
-bot.on('beacon', function (event) {
-//	event.reply('beacon: ' + event.beacon.hwid);
+bot.on('beacon', function(event) {
+    //	event.reply('beacon: ' + event.beacon.hwid);
 });
 
 
@@ -146,13 +165,13 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+    res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+    res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 var port = process.env.PORT || 1337;
@@ -163,3 +182,87 @@ httpServer.listen(port, function() {
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
+
+
+
+function callParseServerCloudCode(methodName, requestMsg, responseMsg) {
+    console.log("callParseServerCloudCode:" + methodName + "\nrequestMsg:" + requestMsg);
+    var options = {
+        url: 'https://reply-msg-parse-server.herokuapp.com/parse/functions/' + methodName,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Parse-Application-Id': 'myAppId',
+            'X-Parse-REST-API-Key': 'myRestKey'
+        },
+        body: requestMsg
+    };
+
+    function callback(error, response, body) {
+        console.log("response:" + JSON.stringify(response));
+        if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            responseMsg(info.result.replyMsg);
+            console.log("result.msg: " + info.result.msg + " result.replyMsg: " + info.result.replyMsg);
+        } else {
+            console.error("Unable to send message. Error :" + error);
+        }
+    }
+    request(options, callback);
+}
+
+function processMessage(reqMsg, resMsg) {
+    if (reqMsg.length > 6) {
+        var checkMsg = reqMsg.substring(0, 4);
+        switch (checkMsg) {
+            case '#ask':
+                // trainingCommand
+                trainingCommand(reqMsg, function(res) {
+                    if (!res) {
+                        resMsg("ข้าว่ามีบางอย่างผิดพลาด ลองใหม่ซิ");
+                        //failed
+                    } else {
+                        resMsg("ข้าจำได้แล้ว ลองทักข้าใหม่ซิ อิอิ");
+                        //success
+                    }
+                });
+                break;
+            case '#bot':
+                // botCommand
+                resMsg("bot command");
+
+                break;
+
+            default:
+                resMsg(reqMsg);
+        }
+    } else {
+        // return original msg
+        resMsg(reqMsg);
+    }
+}
+
+function trainingCommand(msg, res) {
+    msg = msg.replace("#ask ", "");
+    msg = msg.replace(" #ans ", ":");
+    var msgs = msg.split(":");
+    var replyDatas = msgs[1].split(",");
+    replyDatas = JSON.stringify(replyDatas);
+    var data = '{"msg":"' + msgs[0] + '","replyMsg":' + replyDatas + '}';
+    callParseServerCloudCode("botTraining", data, function(response) {
+        console.log(response);
+        res(response);
+    });
+}
+
+function isBotCommand(msg, res) {
+    if (msg.length > 6) {
+        if (msg.substring(0, 4) == "#bot") {
+            res(true);
+        } else {
+            res(false);
+        }
+    } else {
+        res(false);
+    }
+}
